227698520ad0307a8f2686cad2947f97d48e507e
Varun <80062969+varunsainihr04@users.noreply.github.com>
2021-08-09 15:06:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import PIL
+from PIL import Image
+import os
+
+
+width = 800
+source = "input/"
+destination = "output/"
+
+# this is the main working code of ths compressor
+def resize_pic(old_pic, new_pic, width):
+    img = Image.open(old_pic)
+    wpercent = (width/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((width,hsize), PIL.Image.ANTIALIAS)
+    img.save(new_pic)
+
+
+def entire_directory(source, destination, width):
+    files = os.listdir(source)
+
+    i = 0
+    for file in files:
+        i+=1
+        old_pic = source + "/" +file
+        new_pic = destination + "/" +file
+        resize_pic(old_pic, new_pic, width)
+        print(i, "done")
+
+
+entire_directory(source, destination, width)
