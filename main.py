9cdddf8fdf63c9997188744971c655a6a5d86fe9
Varun <80062969+varunsainihr04@users.noreply.github.com>
2022-05-17 17:02:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,143 @@
-import PIL
-from PIL import Image
 import os
+import time
+import subprocess
+from pathlib import Path
+from PIL import Image
+import errno
+import time
+from re import search
+
+print('                                      ██╗███╗░░░███╗░█████╗░░██████╗░███████╗')
+print('                                      ██║████╗░████║██╔══██╗██╔════╝░██╔════╝')
+print('                                      ██║██╔████╔██║███████║██║░░██╗░█████╗░░')
+print('                                      ██║██║╚██╔╝██║██╔══██║██║░░╚██╗██╔══╝░░')
+print('                                      ██║██║░╚═╝░██║██║░░██║╚██████╔╝███████╗')
+print('                                      ╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚═════╝░╚══════╝')
+
+print( )
+print('                  ░█████╗░░█████╗░███╗░░░███╗██████╗░██████╗░███████╗░██████╗░██████╗░█████╗░██████╗░')
+print('                  ██╔══██╗██╔══██╗████╗░████║██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗')
+print('                  ██║░░╚═╝██║░░██║██╔████╔██║██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░██║░░██║██████╔╝')
+print('                  ██║░░██╗██║░░██║██║╚██╔╝██║██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗██║░░██║██╔══██╗')
+print('                  ╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░░░░░██║░░██║███████╗██████╔╝██████╔╝╚█████╔╝██║░░██║')
+print('                  ░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝')
+print( )
+print( )
+time.sleep(0.5)
+print('                   [Help : Less ratio lower will be the size, More ratio better will be the quality]')
+print('                                       [Recommended Ratio : B/w [30% to 80%]')
+print( )
+
+selectquality = input("Enter Image Quality ratio b/w [0 to 100] : ")
+
+CONVERT_PNG_TO_JPG = False
+TOTAL_ORIGINAL = 0
+TOTAL_COMPRESSED = 0
+TOTAL_GAIN = 0
+TOTAL_FILES = 0
+QUALITY = int(selectquality)
 
 
-width = 1080
-source = "input/"
-destination = "output/"
+def compress(location):
+    for r, d, f in os.walk(location):
+        for item in d:
+            compress(location + os.sep + item)
 
-# this is the main working code of ths compressor
-def resize_pic(old_pic, new_pic, width):
-    img = Image.open(old_pic)
-    wpercent = (width/float(img.size[0]))
-    hsize = int((float(img.size[1])*float(wpercent)))
-    img = img.resize((width,hsize), PIL.Image.ANTIALIAS)
-    img.save(new_pic)
+        for image in f:
+            path = location
+            input_path = path + os.sep + image
+            out_path = path.replace(r'input', r'output')
+            if image.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif', 'webp')):
+                if os.path.isfile(input_path):
+                    global TOTAL_GAIN
+                    global TOTAL_ORIGINAL
+                    global TOTAL_COMPRESSED
+                    global TOTAL_FILES
+                    global QUALITY
+                    opt = None
+                    
+                    try:
+                        opt = Image.open(input_path)
+                    except:
+                        #do nothing just print the file skipping
+                        print(f'skipping file cannot open: {input_path}')
+                        continue
+                        
+                    original_size = os.stat(input_path).st_size / 1024 / 1024
+                    TOTAL_ORIGINAL += original_size
+                    print(input_path)
+                    print("Original size: " + f'{original_size:,.2f}' + ' Megabytes')
+                    if not os.path.exists(out_path):
+                        try:
+                            os.makedirs(out_path, exist_ok=True)
+                        except OSError as e:
+                            #wait for race condition to settle
+                            time.sleep(1)
+                            # try to create the folder again
+                            os.makedirs(out_path, exist_ok=True)
+                            if e.errno != errno.EEXIST:
+                                raise
+                    
+                    out_file= out_path + os.sep + image
+                    # Convert .pgn to .jpg
+                    if CONVERT_PNG_TO_JPG and image.lower().endswith('.png'):
+                        im = opt
+                        rgb_im = im.convert('RGB')
+                        out_file = out_file.replace(".png", ".jpg")
+                        rgb_im.save(out_file)
+                        opt = Image.open(out_file)
+                    opt.save(out_file, optimize=True, quality=QUALITY)
+                    opt = Image.open(out_file)
+                    compressed_size = os.stat(out_file).st_size / 1024 / 1024
+                    TOTAL_COMPRESSED += compressed_size
+                    gain = original_size - compressed_size
+                    TOTAL_GAIN += gain
+                    TOTAL_FILES +=1
+                    print("Compressed size: " + f'{compressed_size:,.2f}' + " megabytes")
+                    print("Gain : " + f'{gain:,.2f}' + " megabytes")
+                    opt.close()
+            else:
+                if os.path.isdir(out_path) and not os.path.exists(out_path):
+                    try:
+                        os.makedirs(out_path, exist_ok=True)
+                    except OSError as e:
+                        #wait for race condition to settle
+                        time.sleep(1)
+                        # try to create the folder again
+                        os.makedirs(out_path, exist_ok=True)
+                        if e.errno != errno.EEXIST:
+                            raise
+                if os.path.isfile(input_path):
+                    
+                    if  not os.path.exists(out_path):
+                        try:
+                            os.makedirs(out_path, exist_ok=True)
+                        except OSError as e:
+                            #wait for race condition to settle
+                            time.sleep(1)
+                            # try to create the folder again
+                            os.makedirs(out_path, exist_ok=True)
+                            if e.errno != errno.EEXIST:
+                                raise        
+                    input_file = input_path
+                    output_file= input_file.replace('input','output')        
+                    print('File not image, copying instead: ' + input_path)
+                    subprocess.call('cp ' + input_file + ' ' + output_file, shell=True)
 
 
-def entire_directory(source, destination, width):
-    files = os.listdir(source)
+if __name__ == '__main__':
+    start_path = os.path.dirname(os.path.abspath(__file__)) + os.sep + r"input"
+    
+    # ask if .pgn images should automatically converted to .jpg
+    CONVERT_PNG_TO_JPG = input('Would you like to convert .png images to .jpg? (y/n): ') == 'y'
+    TOTAL_GAIN = 0
+    compress(start_path)
+    print("---------------------------------------------------------------------------------------------")
+    print('-------------------------------------------SUMMARY-------------------------------------------')
+    print('Files: '  + f'{TOTAL_FILES}')
+    print(
+        "Original: " + f'{TOTAL_ORIGINAL:,.2f}' + " megabytes || " + "New Size: " + f'{TOTAL_COMPRESSED:,.2f}' +
+        " megabytes" + " || Gain: " + f'{TOTAL_GAIN:,.2f}' + " megabytes ~" + f'{(TOTAL_GAIN / TOTAL_ORIGINAL) * 100:,.2f}'
+        + "% reduction")
 
-    i = 0
-    for file in files:
-        i+=1
-        old_pic = source + "/" +file
-        new_pic = destination + "/" +file
-        resize_pic(old_pic, new_pic, width)
-        print(i, "image compressed")
-
-
-entire_directory(source, destination, width)
+time.sleep(secs)
